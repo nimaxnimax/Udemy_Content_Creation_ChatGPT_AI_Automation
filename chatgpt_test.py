9d29f409ba2e7dcc42c161be50a1876ad79cf65f
nimaxnimax <avaroti.service@gmail.com>
2024-01-26 07:19:55 +0100

    done

--- chatgpt_test.py
+++ chatgpt_test.py
@@ -1,8 +1,6 @@
 import os
 import openai
 
-prompt = "Hello. How are you?"
-
 def generate_response(question):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -17,12 +15,18 @@ def generate_response(question):
     return(result)
 
 try:      
-    file_name = "chatgpt_api_key.txt"
-    with open(file_name, 'r') as file:
+    prompt = input("Enter your prompt: ")
+    print("You entered:", prompt)
+    with open("chatgpt_api_key.txt", 'r') as file:
         api_key = file.readline().strip()
     openai.api_key = api_key
     zres = generate_response(prompt)
     print(f"{zres}")          
+    with open("Output.txt", "a", encoding="utf-8") as file:
+        file.write("**********" + "\n")
+        file.write(prompt + "\n")
+        file.write("**********" + "\n")
+        file.write(zres + "\n")
+        file.write("\n")
 except:
     print("Error!")
-
